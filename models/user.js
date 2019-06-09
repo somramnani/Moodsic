6c9050626619cdf5c61f4e8e595fc698a014d0c7
Dan Correa <dancorrea3@gmail.com>
2019-06-09 19:10:00 -0400

    Formatted js files to prep for additional work

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ var bcrypt = require("bcryptjs");
 //
 // Creating our User model
 //Set it as export because we will need it required on the server
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   var User = sequelize.define('User', {
     firstname: {
       type: DataTypes.STRING,
@@ -17,31 +17,31 @@ module.exports = function(sequelize, DataTypes) {
       unique: false,
     },
     email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    validate: {
-      isEmail: true
-    }
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
     }
   }, {
     freezeTableName: true,
     hooks: {
-  beforeCreate: function(user) {
-    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
-  }
-} 
-})
-  
+      beforeCreate: function (user) {
+        user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+      }
+    }
+  })
+
   // Creating a custom method for our User model. 
   //This will check if an unhashed password entered by the 
   //user can be compared to the hashed password stored in our database
-  User.prototype.validPassword = function(password) {
-  return bcrypt.compareSync(password, this.password);
+  User.prototype.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
   };
   // Hooks are automatic methods that run during various phases of the User Model lifecycle
   // In this case, before a User is created, we will automatically hash their password
@@ -51,4 +51,4 @@ module.exports = function(sequelize, DataTypes) {
   });
   */
   return User;
-  };
+};
