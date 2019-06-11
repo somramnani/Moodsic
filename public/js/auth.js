9615e575c6600f9624e8c4a5f154e764c6bc1751
Dan Correa <dancorrea3@gmail.com>
2019-06-11 18:56:50 -0400

    Deleted Firebase code on auth.js

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,23 +1,3 @@
-/* eslint-disable prettier/prettier */
-/* eslint-disable no-unused-vars */
-
-//________________________________________________________
-//FIREBASE
-//________________________________________________________
-// var firebaseConfig = {
-//   apiKey: "AIzaSyBnyOQkasRUlKo6Aay1tAswnw0Ij-e5mPE",
-//   authDomain: "moodsic-ad584.firebaseapp.com",
-//   databaseURL: "https://moodsic-ad584.firebaseio.com",
-//   projectId: "moodsic-ad584",
-//   storageBucket: "moodsic-ad584.appspot.com",
-//   messagingSenderId: "376593131880",
-//   appId: "1:376593131880:web:36cf307a01fabd37"
-// };
-
-// firebase.initializeApp(firebaseConfig);
-// console.log("FIREBASE: ", firebase);
-//________________________________________________________
-
 //________________________________________________________
 // GLOBAL VARIABLES
 //________________________________________________________
