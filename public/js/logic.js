3399ea8fde317b3e9eb347d763372f607770298d
Dan Correa <dancorrea3@gmail.com>
2019-06-10 20:06:35 -0400

    Changed all ' to " in logic.js, per lint

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,49 +1,49 @@
 require("./spotify");
 
 window.onSpotifyWebPlaybackSDKReady = () => {
-    const token = 'BQCUmT0Sd-FEFzSlvXqsVD_b5_AaBzP7b63xesbUGSQZmeH-h36T-a5SEVRZwNBp3zy7Rxi3_HNSLM_IRmhB3vSQwu2cvWCgGjgtAck6c-vLmOfYr2GOtbkPONqntIafR9k152bLkwwyuNtzvLGd2g';
+    const token = "BQCUmT0Sd-FEFzSlvXqsVD_b5_AaBzP7b63xesbUGSQZmeH-h36T-a5SEVRZwNBp3zy7Rxi3_HNSLM_IRmhB3vSQwu2cvWCgGjgtAck6c-vLmOfYr2GOtbkPONqntIafR9k152bLkwwyuNtzvLGd2g";
     const player = new Spotify.Player({
-        name: 'Web Playback SDK Quick Start Player',
+        name: "Web Playback SDK Quick Start Player",
         getOAuthToken: cb => {
             cb(token);
         }
     });
     // Error handling
-    player.addListener('initialization_error', ({
+    player.addListener("initialization_error", ({
         message
     }) => {
         console.error(message);
     });
-    player.addListener('authentication_error', ({
+    player.addListener("authentication_error", ({
         message
     }) => {
         console.error(message);
     });
-    player.addListener('account_error', ({
+    player.addListener("account_error", ({
         message
     }) => {
         console.error(message);
     });
-    player.addListener('playback_error', ({
+    player.addListener("playback_error", ({
         message
     }) => {
         console.error(message);
     });
     // Playback status updates
-    player.addListener('player_state_changed', state => {
+    player.addListener("player_state_changed", state => {
         console.log(state);
     });
     // Ready
-    player.addListener('ready', ({
+    player.addListener("ready", ({
         device_id
     }) => {
-        console.log('Ready with Device ID', device_id);
+        console.log("Ready with Device ID", device_id);
     });
     // Not Ready
-    player.addListener('not_ready', ({
+    player.addListener("not_ready", ({
         device_id
     }) => {
-        console.log('Device ID has gone offline', device_id);
+        console.log("Device ID has gone offline", device_id);
     });
     // Connect to the player!
     player.connect();
