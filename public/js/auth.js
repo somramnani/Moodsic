c7fa539add88c930fbbdcb7ccb1949007b89dde5
Som Ramnani <som.ramnani@gmail.com>
2019-06-12 00:19:36 -0500

    Made it so that the account logo and logout button only show when signed in therefore the users can access these buttons unless logged in

--- public/js/auth.js
+++ public/js/auth.js
@@ -18,12 +18,21 @@ $("#signup_div").hide();
 
 // Add login event
 loginBtn.on("click", function () {
-  //Sign in
+  //Hides the login form and fades out
   $("#login_div").addClass("fadeOut displaynone");
   $("#login_div").removeClass(" delay-3s fadeIn");
 
+  // Shows the main body and fades in
   $("#body_div").removeClass("displaynone");
   $("#body_div").addClass("fadeIn");
+
+  // Displays the account logo and fades in
+  $("#account-icon-li").removeClass("displaynone");
+  $("#account-icon-li").addClass("fadeIn");
+ 
+  // Displays the logout and fades in
+  $("#logout-li").removeClass("displaynone");
+  $("#logout-li").addClass("fadeIn");
 });
 
 // Continue as guest
@@ -58,4 +67,12 @@ logOutBtn.on("click", function () {
   $("#login_div").removeClass("fadeIn delay-3s fadeOut displaynone");
   $("#login_div").addClass("fadeIn");
   $("#body_div").addClass("displaynone");
+
+   // Displays the account logo and fades in
+   $("#account-icon-li").addClass("displaynone");
+   $("#account-icon-li").removeClass("fadeIn");
+  
+   // Displays the logout and fades in
+   $("#logout-li").addClass("displaynone");
+   $("#logout-li").removeClass("fadeIn");
 });
